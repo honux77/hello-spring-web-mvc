5166e3267ee30b797a52e9667133a8b0488ab9fa
Hoyoung Jung <honux77@gmail.com>
2021-04-12 08:24:45 +0900

    Add logback

--- src/main/java/honux/HelloApplication.java
+++ src/main/java/honux/HelloApplication.java
@@ -1,7 +1,13 @@
 package honux;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloApplication {
+    private static Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
     public static void main(String[] args) {
+        logger.info("Hello App started..");
         System.out.println("Hello world");
     }
 }
