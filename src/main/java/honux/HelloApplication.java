f1544c668828e21a949b1d8f64f7b8b432ce4b5e
Hoyoung Jung <honux77@gmail.com>
2021-04-13 07:35:42 +0900

    Init Spring context and remove driver class

--- src/main/java/honux/HelloApplication.java
+++ src/main/java/honux/HelloApplication.java
@@ -2,13 +2,20 @@ package honux;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class HelloApplication {
     private static Logger logger = LoggerFactory.getLogger(HelloApplication.class);
 
     public static void main(String[] args) {
         logger.info("Hello App started..");
-        Driver driver = new Driver();
-        driver.drive();
+        //ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml", HelloApplication.class);
+        KoreaTire kt = new KoreaTire();
+        MichelinTire mt = new MichelinTire();
+        Car c1 = new Car(kt);
+        System.out.println(c1.run());
+        Car c2 = new Car(mt);
+        System.out.println(c2.run());
     }
 }
