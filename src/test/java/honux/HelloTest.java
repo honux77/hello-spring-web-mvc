b1fd00f9ceee0068a6e35316913b6643f87869d2
Hoyoung Jung <honux77@gmail.com>
2021-04-13 08:23:51 +0900

    Add spring test

--- src/test/java/honux/HelloTest.java
+++ src/test/java/honux/HelloTest.java
@@ -2,12 +2,23 @@ package honux;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("/config.xml")
 public class HelloTest {
+    @Autowired
+    Car car;
+
     @Test
-    @DisplayName("Junit, AssertJ 정상동작 확인")
-    void hello() {
-        assertThat(new HelloApplication()).isNotNull();
+    void config() {
+        assertThat(car.run()).isNotNull();
+        System.out.println(car.run());
     }
 }
