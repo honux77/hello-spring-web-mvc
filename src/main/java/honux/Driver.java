7bfc05104e13c8c01308a5cff7a02375acb198d6
Hoyoung Jung <honux77@gmail.com>
2021-04-13 07:24:30 +0900

    Add another Tire class

--- src/main/java/honux/Driver.java
+++ src/main/java/honux/Driver.java
@@ -3,7 +3,10 @@ package honux;
 public class Driver {
     public void drive() {
         KoreaTire kt = new KoreaTire();
-        Car car = new Car(kt);
-        System.out.println(car.run());
+        MichelinTire mt = new MichelinTire();
+        Car c1 = new Car(kt);
+        System.out.println(c1.run());
+        Car c2 = new Car(mt);
+        System.out.println(c2.run());
     }
 }
---
+++ src/main/java/honux/MichelinTire.java
@@ -0,0 +1,8 @@
+package honux;
+
+public class MichelinTire implements Tire{
+    @Override
+    public String getBrand() {
+        return "미슐랭";
+    }
+}
